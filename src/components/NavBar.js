f2f993d2b17712e3505800ec6a3e775e9e949bef
Jeremy Kisner <kisner.jeremy@protonmail.com>
2023-05-01 22:51:17 -0400

    updating cart in the ui
    started updating cart to have an icon and a badge

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import CartIcon from "../images/cart.svg"
 
 const NavBar = () => {
-    return <nav className="nav">
-        <Link to="/" className="library-app">Home</Link>
-        <ul>
-            <li>
-                <Link to="/addBook">Add Book</Link>
-            </li>
-            <li>
-                <Link to="/deleteBook">Delete Book</Link>
-            </li>
-            <li>
-                <Link to="/cart">Cart</Link>
-            </li>
-        </ul>
-    </nav>
+    return (
+        <>
+            <nav className="nav">
+                <Link to="/" className="library-app">Home</Link>
+                <ul>
+                    <li>
+                        <Link to="/addBook">Add Book</Link>
+                    </li>
+                    <li>
+                        <Link to="/deleteBook">Delete Book</Link>
+                    </li>
+                    <li>
+                        <Link to="/cart">
+                            <img src={CartIcon} width="25" height="25"></img>
+                            <div className="badge" >0</div>
+                        </Link>
+                        
+                    </li>
+                </ul>
+            </nav>
+        </>
+    )
 }
 
 export default NavBar;
